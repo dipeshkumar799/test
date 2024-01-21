2e913ba3048d6a1e054e8465cf0ee0ba8fbbae5a
dipeshkumar799 <yvdipesh0011@gmail.com>
2024-01-21 13:19:28 -0800

    initial commit

--- routes/notes.js
+++ routes/notes.js
@@ -43,4 +43,81 @@ router.get("/fetchNotes", fetchUser, async (req, res) => {
   res.json(notes);
 });
 
+//for update to no
+router.put("/updateNotes/:_id", fetchUser, async (req, res) => {
+  try {
+    const { title, discripition, tag } = req.body;
+    // Create a new note object
+    const newNotes = {};
+    if (title) {
+      newNotes.title = title;
+    }
+    if (discripition) {
+      newNotes.discripition = discripition;
+    }
+    if (tag) {
+      newNotes.tag = tag;
+    }
+
+    // Find the note to be updated and update it
+    const _id = req.params._id; // Corrected from const {_id}=req.params._id;
+    const notes = await Notes.findById(_id);
+
+    // Check if the user is authorized to update the note
+    if (notes.user.toString() !== req.user.id) {
+      return res.status(403).json({
+        error: "You are not allowed to update this note.",
+      });
+    }
+
+    const updatedNote = await Notes.findByIdAndUpdate(
+      _id,
+      { $set: newNotes },
+      { new: true }
+    );
+
+    if (!updatedNote) {
+      return res.status(404).json({
+        error: "Note not found.",
+      });
+    }
+
+    res.json(updatedNote);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: "Internal Server Error. Unable to update the note.",
+    });
+  }
+});
+
+router.delete("/deleteNote/:_id", fetchUser, async (req, res) => {
+  try {
+    const _id = req.params._id;
+    const note = await Notes.findById(_id);
+
+    if (note.user.toString() !== req.user.id) {
+      return res.status(403).json({
+        error: "You are not allowed to delete this note.",
+      });
+    }
+    const deleteNote = await Notes.findByIdAndDelete(_id);
+    if (!deleteNote) {
+      return res.status(404).json({
+        error: "Note not found.",
+      });
+    }
+
+    res.json({
+      message: "Note deleted successfully",
+      deleteNote: deleteNote,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: "Internal Server Error. Unable to delete the note.",
+    });
+  }
+});
+
 export default router;
